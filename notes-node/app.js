55f159c49d601bcd96ff447bf6df0a9cde046e91
Yuriy <ystos@uptimehealth.com>
2018-12-21 17:51:18 +0200

    Delete unneccessarry staff

--- notes-node/app.js
+++ notes-node/app.js
@@ -1,17 +1,6 @@
 console.log('App has started.');
 
 const fs = require('fs');
-const os = require('os');
 const _ = require('lodash');
+
 const notes = require('./notes.js');
-
-var user = os.userInfo();
-
-console.log(_.isString(user.username));
-
-console.log(notes.add(1, -1));
-
-// fs.appendFile('greetings.txt', `Hello ${user.username}! You are ${notes.age}`, (err) => {
-//     if (err) throw err;
-//     console.log(notes.addNote());
-// });
