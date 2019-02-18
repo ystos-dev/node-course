5c367d0cd1394e2f7fc05f5ad6ea955f7f61661a
Yuriy <ystos@uptimehealth.com>
2019-02-18 15:43:37 +0200

    Add express

--- Dockerfile
+++ Dockerfile
@@ -1,33 +1,10 @@
-FROM node:10.14.2
+FROM node:10.14.2  
 
-WORKDIR /opt/app
-COPY . .
+WORKDIR /opt/app 
+COPY . .  
+RUN npm install 
+RUN npm -g config set user root
+RUN npm -g install nodemon
+# RUN npm install nodemon -g  
 
-RUN npm install
-RUN npm install nodemon -g
-
-## Installing  pdftk
-#RUN apt-get update && \
-#    apt-get install -y pdftk && \
-#    ln -s /usr/bin/pdftk /usr/local/bin/pdftk
-#
-## Copying project to proper folder
-#COPY . .
-#
-## Installing additional packages
-#RUN npm install node-pdftk && \
-#    npm install gm && \
-#    npm install pm2 -g && \
-#    npm install sails-mysql && \
-#    npm install sails && \
-#    npm install node-pdftk && \
-#    npm install
-#
-## Added ability to execute startup.sh file
-#RUN chmod +x ./start.sh
-#
-## Exposing ports
-#EXPOSE 3000 1337
-#
-## Starting application
-#ENTRYPOINT [ "./start.sh" ]
+EXPOSE 3001
---
+++ server.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+
+app.use((req, rest, next) => {
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`;
+    fs.appendFile('server.log', log + '\n', (err) => {
+        console.log(err);
+    });
+    next();
+});
+
+app.set('view engine', 'hbs');
+
+app.use(express.static(__dirname + '/public'));
+
+app.use((req, resp, next) => {
+    resp.render('maitanance.hbs', {
+        pageTitle: 'Under the maitanance',
+        welcomeMessage: "Please, wait for a while"
+    })
+});
+
+app.get('/', (request, response) => {
+    response.render('home.hbs', {
+        pageTitle: 'About page',
+        welcomeMessage: "Welcome"
+    });
+});
+
+app.get('/about', (request, response) => {
+    response.render('about.hbs', {
+        pageTitle: 'About page'
+    });
+})
+
+app.get('/bad', (request, response) => {
+    response.send({
+        error: "Error handling request"
+    });
+})
+
+app.listen(3001);
